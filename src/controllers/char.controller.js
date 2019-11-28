0b406b06c24186433de673c0d6597743820e3496
Nam Lê Trung <yentth237@gmail.com>
2019-11-29 00:01:03 +0700

    add line chart

--- src/controllers/char.controller.js
+++ src/controllers/char.controller.js
@@ -3,15 +3,26 @@ const Spends = require("../models/spend.model");
 module.exports = {
   index: async (req, res) => {
     const idGoogle = req.signedCookies.userId;
+    // data pie-chart
     const spends = await Spends.find({ idGoogle });
+
     let money = [];
     let danhmucs = [];
     let tongS = 0;
+    let dates = [];
     for (let item = 0; item < spends.length; item++) {
       money.push(spends[item].money);
       danhmucs.push(spends[item].danhMuc);
       tongS += money[item];
+      dates.push(
+        `${spends[item].date.getDate().toString()}/${spends[item].date
+          .getMonth()
+          .toString()}`
+      );
     }
-    res.render("chart/index", { money, danhmucs, tongS });
+    danhmucs = JSON.stringify(danhmucs);
+    dates = JSON.stringify(dates);
+
+    res.render("chart/index", { money, danhmucs, tongS, dates });
   }
 };
